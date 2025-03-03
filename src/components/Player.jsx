f2609ed5231703db124bb7203e0c2be5aea98b83
Luthfi <kankagate@gmail.com>
2025-03-03 07:24:30 +0700

    Player phase 1 done

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,52 @@
+import { useState, useRef } from "react";
 export default function Player({ name, symbol, isActive }) {
-  let isEdit = false;
-  //   let isActive = true;
+  // let isEdit = false;
+  const [isEdit, setIsEdit] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+  const inputRef = useRef(null);
+
+  console.log("============");
+  console.log("isEdit ", isEdit);
+  console.log("============");
+
+  //html element to display the player name
+  let playerElement = <span className="player-name">{playerName}</span>;
+  if (isEdit) {
+    playerElement = (
+      <input
+        ref={inputRef}
+        type="text"
+        className="player-name"
+        required
+        value={playerName}
+        onChange={(e) => setPlayerName(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            console.log("Enter key pressed");
+            handleToggleEdit();
+          }
+        }}
+      />
+    );
+  }
+
+  const handleToggleEdit = () => {
+    setIsEdit((prev) => !prev);
+    console.log("isEdit in handleToggleEdit", isEdit);
+    if (isEdit) {
+      setPlayerName((prevName) => prevName.trim() || name); // Prevent empty name
+    } else {
+      setTimeout(() => inputRef.current?.focus(), 0); // Auto-focus input
+    }
+  };
+
   return (
     <li className={isActive ? "active" : undefined}>
       <span className="player">
-        {name}
+        {playerElement}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>{isEdit ? "Save" : "Edit"}</button>
+      <button onClick={handleToggleEdit}>{isEdit ? "Save" : "Edit"}</button>
     </li>
   );
 }
